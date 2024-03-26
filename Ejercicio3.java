defe21ac35d856b3232a8f859fcb9dbafc7aa362
Jancarlos042003 <U21302940@utp.edu.pe>
2024-03-25 22:27:01 -0500

    Se eliminó los ejercicios movidos

--- Ejercicio3.java
+++
@@ -1,8 +0,0 @@
-/*
- Escribe un programa que resuelva una ecuación de segundo grado 
- ax2 + bx + c = 0 y devuelva las raíces reales ordenadas ascendentemente.
-*/
-
-public class Ejercicio3 {
-    
-}
